cbee5b5f0a3425b92bf41ab32d5725725a075832
James Nord <jnord+github@cloudbees.com>
2015-06-11 15:58:59 +0100

    update javadoc for RunIdStore

--- src/main/java/org/jenkinsci/plugins/uniqueid/implv2/RunIdStore.java
+++ src/main/java/org/jenkinsci/plugins/uniqueid/implv2/RunIdStore.java
@@ -8,7 +8,10 @@ import hudson.model.Run;
 import org.jenkinsci.plugins.uniqueid.IdStore;
 
 /**
- * Manages Unique IDs for Runs.
+ * Manages Unique IDs for Runs. 
+ * Whilst we could use the {@link PersistenceRootIdStore} that will create extra files for
+ * every single build. A build already has a unique identifier (build number / build id) and a parent job can have a
+ * unique ID, so we build one from the parent and our build number to save creating a file.
  */
 @Extension(ordinal=1) // needs to take priority over the PersistenceRootIdStore
 public class RunIdStore extends IdStore<Run> {
